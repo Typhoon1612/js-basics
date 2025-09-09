bc6f0243450833286df253903a3724515ff7a6fe
Typhoon1612 <119401052+Typhoon1612@users.noreply.github.com>
2025-09-09 12:12:59 +0800

    9/9/2025 1212

--- fibonacciNum.js
+++ fibonacciNum.js
@@ -1,6 +1,6 @@
 function fibonacciGenerator(n) {
-    let arr = [0]
-    for (let i = -1; i < n - 2; i++){
+    let arr = [0];
+    for (let i = -1; i < n - 2; i++) {
         let num = arr.length === 1 ? arr[0] + 1 : arr[i] + arr[i + 1];
         arr.push(num);
     }
